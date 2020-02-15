bae15cdcafa195de1cae8e5d4ec78e3628f0a9c0
Linder <vhelyno@gmail.com>
2020-02-15 17:52:33 +0100

    update

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import Article from '../models/Article';
-import { environment } from 'src/environments/environment';
+import { environment } from 'environments/environment';
 
 const apiUrl = `${environment.rootUrl}/articles`;
 
